cc873ad8618941ab89400ab61e79daa7d1cbb7c0
avpellic <arthur.vanpellicom@ugent.be>
2020-01-03 13:32:14 +0100

    Start payment service

---
+++ sportarena-app-payment/src/main/java/be/ugent/groep10/payment/SportarenaAppPaymentApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.groep10.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportarenaAppPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportarenaAppPaymentApplication.class, args);
+	}
+
+}

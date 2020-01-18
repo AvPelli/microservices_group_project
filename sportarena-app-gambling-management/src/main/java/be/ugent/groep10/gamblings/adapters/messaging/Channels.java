072130a903635c8427224ac2ddbb161cf7d4aa26
Avpellic <Arthur.VanPellicom@ugent.be>
2020-01-18 12:30:21 +0100

    Aanpassen channels in bettingservice

--- sportarena-app-gambling-management/src/main/java/be/ugent/groep10/gamblings/adapters/messaging/Channels.java
+++ sportarena-app-gambling-management/src/main/java/be/ugent/groep10/gamblings/adapters/messaging/Channels.java
@@ -6,7 +6,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface Channels {
-	static final String NEW_EVENT = "events";
+	static final String GAME_CREATED_EVENT = "game_created_event";
 	static final String EVENT_REGISTERED  = "registered_events";
 	
 	static final String CREATE_WALLET = "create_wallet";
@@ -17,7 +17,7 @@ public interface Channels {
 	
 	
 	//Arena mgmt
-	@Input(NEW_EVENT)
+	@Input(GAME_CREATED_EVENT)
 	SubscribableChannel newEvent();
 	
 	@Output(EVENT_REGISTERED)

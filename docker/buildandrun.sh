c90cf265c4ff951babcf955b8570accaf8b4dfa0
Jasper Maes <jantmaes.maes@ugent.be>
2020-01-20 06:25:11 +0100

    cleaned version

--- docker/buildandrun.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "[INF] \t Building and starting containers (might take some time)"
-docker-compose up -d --build --no-color >& docker-compose.log
-echo -e "[INF] \t Docker-compose finished, logs written to docker-compose.log"
